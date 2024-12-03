0fc667a4cf2e149a8e98f2d39e7a04273fc0ca87
Sane Aalam <saneraza78692@gmail.com>
2024-12-03 16:25:43 +0530

    Episode-01 | Inception 🚀

---
+++ Episode 01 - Inception/Code/App.js
@@ -0,0 +1,15 @@
+/* 
+
+<div id="parent">
+  <div id=child>
+    <h1>I'm h1 Tag</h1>
+    <h2>I'm h2 Tag</h2>
+  </div>
+  <div id=child2>
+    <h1>I'm h1 Tag</h1>
+    <h2>I'm h2 Tag</h2>
+  </div>
+</div>
+
+ReactElement(Object) => HTML(Browser Understands) 
+*/
